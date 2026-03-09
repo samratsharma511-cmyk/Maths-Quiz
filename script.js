37901a831af7d1a4c825d5c65b403c1f3314c76b
samrat sharma <samratsharma@samrats-MacBook-Pro.local>
2026-03-09 20:36:43 +0530

    Lund

--- script.js
+++ script.js
@@ -1,30 +1,82 @@
-let randomNum1 = Math.floor(Math.random() * 10);
-let randomNum2 = Math.floor(Math.random() * 10);
-let a = document.getElementById('num1');
-let b = document.getElementById('num2');
-a.innerText = randomNum1;
-b.innerText = randomNum2;
-let inp = document.getElementById('answerinput');
-let btn = document.getElementById('btn');
-let score = document.getElementById('score');
-let s = 0;
-btn.addEventListener('click', () => {
-    let correctAnswer = randomNum1 * randomNum2;
-    if (inp.value == correctAnswer) {
-        s += 1;
+let randomNum1=Math.floor(Math.random()*10);
+let randomNum2=Math.floor(Math.random()*10);
+let a = document.getElementById("num1");
+let b = document.getElementById("num2");
+a.innerText=randomNum1;
+b.innerText=randomNum2;
+let math = document.getElementById("math");
+let operator;
+let result;
+let temp = document.getElementById("math op");
+let arr1=["divide","plus","minus","multiply"];
+let r1 = Math.floor(Math.random()*4);
+operator=arr1[r1];
+math.innerText=operator;
+if(operator== "divide")
+{
+    result = randomNum1/randomNum2;
+    temp.innerText="by";
+}
+else if(operator=="plus")
+{
+    result = randomNum1+randomNum2;
+    temp.innerText="";
+}
+else if(operator=="minus")
+{
+    result = randomNum1-randomNum2;
+    temp.innerText="";
+}
+else if(operator == "multiply")
+{
+    result = randomNum1*randomNum2;
+    temp.innerText="by";
+}
+let s=0;
+let button = document.getElementById("btn");
 
-        confetti({
-            particleCount: 1000,
-            spread: 300,
-            origin: {y: 0.5 },
-        });
-    } else {
-        s -= 1;
+button.addEventListener("click",function(){
+    let inp = document.getElementById("answerinput");
+    if(result==Number(inp.value))
+    {
+        s+=1;
     }
-    score.textContent = s;
-    randomNum1 = Math.floor(Math.random() * 10);
-    randomNum2 = Math.floor(Math.random() * 10);
+    else{
+        s-=1;
+    }
+    let score = document.getElementById("score");
+    score.innerText=s;
+
+    randomNum1 = Math.floor(Math.random()*10);
+    randomNum2 = Math.floor(Math.random()*10);
     a.innerText = randomNum1;
     b.innerText = randomNum2;
-    inp.value = '';
-});
+
+    let arr1=["divide","plus","minus","multiply"];
+    let r1 = Math.floor(Math.random()*4);
+    operator=arr1[r1];
+    math.innerText=operator;
+    let temp = document.getElementById("math op");
+    
+    if(operator== "divide")
+    {
+        result = randomNum1/randomNum2;
+        temp.innerText="by";
+    }
+    else if(operator=="plus")
+    {
+        result = randomNum1+randomNum2;
+        temp.innerText="";
+    }
+    else if(operator=="minus")
+    {
+        result = randomNum1-randomNum2;
+        temp.innerText="";
+    }
+    else if(operator == "multiply")
+    {
+        result = randomNum1*randomNum2;
+        temp.innerText="by";
+    }
+    inp.value="";
+})
